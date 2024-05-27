b41cf6b1be69d0cdb777c55419d0529ea56be542
Matheus <matheusbezerra18@gmail.com>
2024-05-26 22:30:47 -0300

    Feat: dinamic header and cleaning code

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function dropDown(x) {
-    x.classList.toggle("change");
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("menu-mobile");
+    x.classList.toggle("menu-button-selected");
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("mobile-menu");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    let lastScrollY = window.scrollY;
+    const header = document.querySelector('header');
+    const mobile = document.getElementById('mobile-elements');
+    let menu = mobile.nextElementSibling;
+    const button = document.getElementById('menu-button');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > lastScrollY) {
+        // Rolagem para baixo
+        header.classList.add('hidden-header');
+        if (menu.classList.contains('mobile-menu')) {
+            dropDown(button);
+        }
+      } else {
+        // Rolagem para cima
+        header.classList.remove('hidden-header');
+      }
+      lastScrollY = window.scrollY;
+    });
+  });
+  
