891dbf25c3390430d104009d9f37595cf9105bbb
Matheus <matheusbezerra18@gmail.com>
2024-06-11 17:27:04 -0300

    implements the sending of orders to whatsapp

--- scripts/productPage.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sizeOptions = document.querySelectorAll('.size-option');
-    const sizeSelector = document.getElementById('sizeSelector');
-});
